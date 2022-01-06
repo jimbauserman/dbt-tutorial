bb74c8791bbfb69bc4f97d39175ec1148a0459fa
jim.bauserman@gmail.com <jim.bauserman@gmail.com>
2022-01-06 15:43:04 +0000

    set up validation on staging jaffle shop

---
+++ target/compiled/jaffle_shop/models/staging/jaffle_shop/stg_jaffle_shop.yml/accepted_values_stg_orders_82a9553b2a8c61d1c339eafa45800497.sql
@@ -0,0 +1,22 @@
+
+    
+    
+
+with all_values as (
+
+    select
+        status as value_field,
+        count(*) as n_records
+
+    from `dbt-fundamentals-337218`.`dbt_jbauserman`.`stg_orders`
+    group by status
+
+)
+
+select *
+from all_values
+where value_field not in (
+    'shipped','completed','returned_pending','returned','placed'
+)
+
+
